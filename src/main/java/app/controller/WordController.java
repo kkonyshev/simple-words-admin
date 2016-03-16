2f62a93232cda930af4e386227eb72c60340e426
Konstantin <hs@ngs.ru>
2016-03-16 21:29:24 +0600

    select ok but...

--- src/main/java/app/controller/WordController.java
+++ src/main/java/app/controller/WordController.java
@@ -5,6 +5,8 @@ import app.model.WordType;
 import app.repository.WordRepository;
 import app.service.WordService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -27,18 +29,19 @@ public class WordController {
 
     @RequestMapping(value = "/words")
     public Iterable<Word> list() {
-        return wordRepository.findAll();
+        Sort sort = new Sort(Sort.Direction.ASC, "value");
+        return wordRepository.findAll(sort);
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/words")
     public Word add(@RequestBody Word word) {
-        return wordService.create(word.value, word.type);
+        return wordService.create(word.value.toUpperCase(), word.type);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/words/{id}")
     public Word update(@PathVariable("id") Long id, @RequestBody Word word) {
         Word localWord = wordRepository.findOne(id);
-        localWord.value = word.value;
+        localWord.value = word.value.toUpperCase();
         localWord.type = word.type;
         return wordRepository.save(localWord);
     }
